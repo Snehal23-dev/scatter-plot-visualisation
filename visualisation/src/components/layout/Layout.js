01142c372764c0b73cf00a306eed367bc82d0cd4
Snehal <snehaldeshmukh233@gmail.com>
2023-03-26 12:16:57 +0530

    Created responsive layout

---
+++ visualisation/src/components/layout/Layout.js
@@ -0,0 +1,11 @@
+import { Container } from "@mui/material";
+
+function Layout() {
+    return (
+        <Container maxWidth={false} disableGutters={true} sx={{ height: '100vh' }}>
+            
+        </Container>
+    )
+}
+
+export default Layout;
